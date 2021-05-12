d3012d9fb57c3cb8db3c6e38ec03f29a1c432f48
Swapnil Mane <swapnilmanew@gmail.com>
2021-05-12 10:46:22 +0000

    sdf

---
+++ anime/src/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-sm-6 col-md-4 m-auto">
+                        <div className="card">
+                            <div className="card-body">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
