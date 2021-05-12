f86b5714565469d6b377247bf3b608d92df78150
Swapnil Mane <swapnilmanew@gmail.com>
2021-05-12 11:08:17 +0000

    asdf

--- anime/src/Home.jsx
+++ anime/src/Home.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 const Home = () => {
     const [anime, setAnime] = useState([]);
 
     const getAnime = async()=>
     {
-        const animeList = await fetch("https://api.jikan.moe/v3/search/anime?q=DeathNode");
+        console.log("hello");
+        const animeList = await fetch("https://api.jikan.moe/v3/search/anime?q=parasyte");
         const animes = await animeList.json();
-        setAnime(animes);
+        setAnime(animes.results);
+        console.log(animes);
     }
     useEffect(() => {
         getAnime();
@@ -17,18 +19,26 @@ const Home = () => {
             <div className="container">
                 <div className="row">
                   {
-                      anime.map((item, key)=>
+                      anime.map((item)=>
                   {
                       return(
                           <>
-      <div className="col-12 col-sm-6 col-md-4 m-auto"}>
-                        <div className="card border-0 shadow">
-                            <img src={item.image_url} alt="" className="card-img-top"/>
-                            <div className="card-body">
-
-                            </div>
-                        </div>
-                    </div>
+                                <div className="col-12 col-sm-6 col-md-4 mx-auto my-4">
+                                    <div className="card border-0 shadow">
+                                        <img src={item.image_url} alt={item.image_url} className="card-img-top"/>
+                                        <div className="card-body">
+                                        <div className="card-title text-center">
+                                            <h4>
+                                            {item.title}
+                                            </h4>
+                                            <p><b>Episodes : </b>{item.episodes}</p>
+                                            <p><b>Score : </b>{item.score}</p>
+                                            <p><b>Episodes : </b>{item.episodes}</p>
+                                            <p><b>Episodes : </b>{item.episodes}</p>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
                           </>
                       )
                   })
