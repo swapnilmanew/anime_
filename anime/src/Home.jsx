85b5c5e266dbd311a0cdeb143c4b8f2df0271094
Swapnil Mane <swapnilmanew@gmail.com>
2021-05-12 10:51:36 +0000

    sdf

--- anime/src/Home.jsx
+++ anime/src/Home.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
 
 const Home = () => {
+    const [anime, setAnime] = useState([]);
+
+    const getAnime = async()=>
+    {
+        const animeList = await fetch("https://api.jikan.moe/v3/search/anime?q=DeathNode");
+        const animes = await animeList.json();
+        setAnime(animes);
+    }
+    useEffect(() => {
+        getAnime();
+    }, [])
     return (
         <>
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-sm-6 col-md-4 m-auto">
-                        <div className="card">
+                  {
+                      anime.map((item, key)=>
+                  {
+                      return(
+                          <>
+      <div className="col-12 col-sm-6 col-md-4 m-auto"}>
+                        <div className="card border-0 shadow">
+                            <img src={item.image_url} alt="" className="card-img-top"/>
                             <div className="card-body">
-                                
+
                             </div>
                         </div>
                     </div>
+                          </>
+                      )
+                  })
+                  }
                 </div>
             </div>
         </>
